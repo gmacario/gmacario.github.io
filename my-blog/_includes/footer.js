784d0d5c6fa6dbaf5743ccac8390e8ac5d235923
GGn0 <luigi.s1994@gmail.com>
2021-05-09 12:15:32 +0200

    Make basic header and footer components

---
+++ my-blog/_includes/footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <footer>
+            <p>
+                @2021 | Footer
+            </p>
+        </footer>
+    )
+}
---
+++ my-blog/_includes/header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <p>
+                Blog | Subtitle
+            </p>
+        </header>
+    )
+}
