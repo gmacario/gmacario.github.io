79dfc2b0676f48e7b6d69bd17d62f82bbd09b7c5
Luigi Scalzone <luigi.scalzone@arol.com>
2021-05-10 12:11:33 +0200

    Fix header getting behing <ul> elements

--- my-blog/_includes/header.js
+++ my-blog/_includes/header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="py-3 fixed top-0 flex flex-wrap justify-between w-screen bg-white border-b border-gray-200">
+        <header className="py-3 fixed top-0 z-50 flex flex-wrap justify-between w-screen bg-white border-b border-gray-200">
             <div className="mx-3 py-2 px-4  font-bold text-indigo-500">
                 Gianpaolo Macario's blog
             </div>
