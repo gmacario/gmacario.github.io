c0e016eea6c668314032f939a5769bed51b92adb
Luigi Scalzone <luigi.scalzone@arol.com>
2021-05-12 13:10:13 +0200

    Make footer responsive

--- _includes/footer.js
+++ _includes/footer.js
@@ -1,40 +1,43 @@
 // _includes/footer.js
-import Link from 'next/link';
-import GithubIcon from '@material-ui/icons/GitHub';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import Link from 'next/link'
+import GithubIcon from '@material-ui/icons/GitHub'
+import TwitterIcon from '@material-ui/icons/Twitter'
 
-export default function Footer(props) {
-    return (
-        <footer className="h-60 mt-20 bg-gray-100 justify-between border-t border-gray-200">
-            <div className="pt-6 mx-auto w-9/12 flex flex-col break-all prose prose-indigo hover:prose-black md:prose-lg lg:prose-xl">
-                <div className="mx-6 prose-xl">
-                    <p className="">
-                        {props.title}
-                    </p>
-                </div>
-                <div className="mx-6 flex flex-row justify-between">
-                    <p className="text-gray-600 prose-sm">
-                        {props.title}
-                    </p>
-                    <div className="ml-3 flex flex-col justify-start">
-                        <p className="text-gray-600 prose-sm flex flex-row">
-                                        <GithubIcon className="mr-2"/>
-                                        <Link href={props.social.github.link}>
-                                            <a>{props.social.github.name}</a>
-                                        </Link>
-                        </p>
-                        <p className="text-gray-600 prose-sm flex flex-row">
-                                        <TwitterIcon className="mr-2"/>
-                                        <Link href={props.social.twitter.link}>
-                                            <a>{props.social.twitter.name}</a>
-                                        </Link>
-                        </p>
-                    </div>
-                    <p className="text-gray-600 prose-sm">
-                        {props.description}
-                    </p>
-                </div>
-            </div>
-        </footer>
-    )
+export default function Footer (props) {
+  return (
+  /* outer box, gray with horizontal line */
+    <footer className='h-auto mt-20 bg-gray-100 justify-between border-t border-gray-200'>
+      {/* Column; Blog title on top, then contact info */}
+      <div className='pt-6 mx-auto w-9/12 flex flex-col prose prose-indigo hover:prose-black md:prose-lg lg:prose-xl'>
+        <div className='mx-6 prose-xl'>
+          <p className=''>
+            {props.title}
+          </p>
+        </div>
+        {/* Col; contact info - over a certain screen size, becomes a column */}
+        <div className='mr-6 sm:mx-6 flex flex-col sm:flex-row flex-nowrap justify-between md:justify-start'>
+          <p className='ml-6 sm:mx-0 text-gray-600 prose-sm'>
+            {props.title}
+          </p>
+          <div className='ml-6 sm:ml-12 flex flex-col justify-start'>
+            <p className='text-gray-600 prose-sm flex flex-row'>
+              <GithubIcon className='mr-2' />
+              <Link href={props.social.github.link}>
+                <a>{props.social.github.name}</a>
+              </Link>
+            </p>
+            <p className='text-gray-600 prose-sm flex flex-row'>
+              <TwitterIcon className='mr-2' />
+              <Link href={props.social.twitter.link}>
+                <a>{props.social.twitter.name}</a>
+              </Link>
+            </p>
+          </div>
+          <p className='ml-6 sm:ml-12 text-gray-600 prose-sm'>
+            {props.description}
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
